8948bc25a8ea1be33e28ab636e1cc1ad31f4ed1d
Marcelo-Diament <marcelodiament@gmail.com>
2022-03-27 11:07:26 -0300

    feat: [#M2A02-pt04] Executando subtrair em calculadora.js

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -4,3 +4,5 @@ const multiplicar = require('./multiplicar')
 const dividir = require('./dividir')
 
 console.log(`Função somar recebendo 1 e 2 retorna: ${somar(1,2)}`)
+
+console.log(`Função subtrair recebendo 10 e 2 retorna: ${subtrair(10,2)}`)
--- Calculadora/subtrair.js
+++ Calculadora/subtrair.js
@@ -1,4 +1,3 @@
 const subtrair = (num1, num2) => num1 - num2
-console.log(`Função subtrair recebendo 10 e 2 retorna: ${subtrair(10,2)}`)
 
 module.exports = subtrair
