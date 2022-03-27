e6ca722f0e8bd09543153e6c50954d1bf1136e31
Marcelo-Diament <marcelodiament@gmail.com>
2022-03-27 12:18:17 -0300

    feat: [#M2A03-pt02] Adicionando função listarTodosOsFilmes

--- CineHouse/cinema.js
+++ CineHouse/cinema.js
@@ -45,3 +45,18 @@ const testarAlterarStatusEmCartaz = () => {
   alterarStatusEmCartaz(2)
 }
 // testarAlterarStatusEmCartaz()
+
+const listarTodosOsFilmes = () => {
+  for (let filme of catalogo) {
+    console.log(`
+      Título: ${filme.titulo}
+      Código: ${filme.codigo}
+      Ano: ${filme.anoDeLancamento}
+      Duração (hs): ${filme.duracao}
+      Atores: ${filme.atores.join(', ')}
+      Status: ${filme.emCartaz ? "Em cartaz" : "Não está em cartaz"}
+    `)
+  }
+}
+const testarListarTodosOsFilmes = () => listarTodosOsFilmes()
+// testarListarTodosOsFilmes()
