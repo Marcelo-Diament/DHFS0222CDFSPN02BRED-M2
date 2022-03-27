ede0c3835999c09d03e28d673333f55d919cd626
Marcelo-Diament <marcelodiament@gmail.com>
2022-03-27 09:44:08 -0300

    feat: [#M2A02-pt02] Adicionando função buscarFilme e testando

--- CineHouse/cinema.js
+++ CineHouse/cinema.js
@@ -1,6 +1,5 @@
 const cinema = "CineMarco"
-
-console.log(cinema)
+// console.log(cinema)
 
 const catalogo = [
   {
@@ -25,13 +24,26 @@ const adicionarFilme = filme => catalogo.push(catalogo)
 const testarAdicionarFilme = () => {
   console.log(`Antes da adição do novo filme ao catálogo, o catálogo possui ${catalogo.length} filmes.`)
   adicionarFilme({
-      codigo: 3,
-      titulo: "Cidadão Kane",
-      duracao: 1.98,
-      atores: ["Orson Welles", "Joseph Cotten", "Dorothy Comingore", "William Alland"],
-      anoDeLancamento: 1941,
-      emCartaz: false
+    codigo: 3,
+    titulo: "Cidadão Kane",
+    duracao: 1.98,
+    atores: ["Orson Welles", "Joseph Cotten", "Dorothy Comingore", "William Alland"],
+    anoDeLancamento: 1941,
+    emCartaz: false
   })
   console.log(`Após adição do novo filme ao catálogo, o catálogo possui ${catalogo.length} filmes.`)
 }
 // testarAdicionarFilme()
+
+const buscarFilme = codigo => {
+  for (let filme of catalogo) {
+    if (filme.codigo === codigo) {
+      console.log(`O filme de código ${codigo} se chama ${filme.titulo}`)
+    }
+  }
+}
+const testarBuscarFilme = () => {
+  buscarFilme(1)
+  buscarFilme(2)
+}
+// testarBuscarFilme()
