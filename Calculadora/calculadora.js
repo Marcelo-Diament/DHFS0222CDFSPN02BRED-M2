5d844f71713867958dc598a653664d4f09cc7dad
Marcelo-Diament <marcelodiament@gmail.com>
2022-03-27 11:07:58 -0300

    feat: [#M2A02-pt04] Executando multiplicar em calculadora.js

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -6,3 +6,8 @@ const dividir = require('./dividir')
 console.log(`Função somar recebendo 1 e 2 retorna: ${somar(1,2)}`)
 
 console.log(`Função subtrair recebendo 10 e 2 retorna: ${subtrair(10,2)}`)
+
+console.log(`Função multiplicar recebendo 10 e 2 retorna: ${multiplicar(10,2)}`)
+console.log(`Função multiplicar recebendo 10 e 0 retorna: ${multiplicar(10,0)}`)
+console.log(`Função multiplicar recebendo 0 e 10 retorna: ${multiplicar(0,10)}`)
+console.log(`Função multiplicar recebendo 0 e 0 retorna: ${multiplicar(0,0)}`)
