7ad6c71ce6b9c1e8efacf629a9f2b727276f00ee
Marcelo-Diament <marcelodiament@gmail.com>
2022-03-27 11:06:39 -0300

    feat: [#M2A02-pt04] Executando somar em calculadora.js

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -2,3 +2,5 @@ const somar = require('./somar')
 const subtrair = require('./subtrair')
 const multiplicar = require('./multiplicar')
 const dividir = require('./dividir')
+
+console.log(`Função somar recebendo 1 e 2 retorna: ${somar(1,2)}`)
--- Calculadora/somar.js
+++ Calculadora/somar.js
@@ -1,4 +1,3 @@
 const somar = (num1, num2) => num1 + num2
-console.log(`Função somar recebendo 1 e 2 retorna: ${somar(1,2)}`)
 
 module.exports = somar
