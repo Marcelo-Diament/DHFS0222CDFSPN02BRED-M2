a4bde4db241a206ad751136fd70c7f5816833799
Marcelo-Diament <marcelodiament@gmail.com>
2022-03-27 10:42:32 -0300

    feat: [#M2A02-pt03] Adicionando função dividir com condicional e testando

---
+++ Calculadora/dividir.js
@@ -0,0 +1,12 @@
+const dividir = (num1, num2) => num2 === 0 ? "Não se pode dividir por zero" : num1 * num2
+/**
+ * NOTA DE ESCLARECIMENTO 
+ * O exercício pede para retornarmos "Não se pode dividir por zero" caso qualquer um dos dois números seja 0.
+ * No entanto, não se pode dividir um número por 0, mas é possível dividir 0 por qualquer número (que não seja 0).
+ */
+console.log(`Função dividir recebendo 10 e 2 retorna: ${dividir(10,2)}`)
+console.log(`Função dividir recebendo 10 e 0 retorna: ${dividir(10,0)}`)
+console.log(`Função dividir recebendo 0 e 10 retorna: ${dividir(0,10)}`)
+console.log(`Função dividir recebendo 0 e 0 retorna: ${dividir(0,0)}`)
+
+module.exports = dividir
