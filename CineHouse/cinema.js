196cd172a41f3a0d2f48a04b8de67341caa3afb2
Marcelo-Diament <marcelodiament@gmail.com>
2022-03-27 09:33:43 -0300

    feat: [#M2A02-pt01] Adicionando Array catalogo com 2 filmes

--- CineHouse/cinema.js
+++ CineHouse/cinema.js
@@ -1,3 +1,22 @@
 const cinema = "CineMarco"
 
 console.log(cinema)
+
+const catalogo = [
+  {
+    codigo: 1,
+    titulo: "O Poderoso Chefão",
+    duracao: 2.91,
+    atores: ["Al Pacino", "Marlon Brando", "Diane Keaton", "Robert De Niro"],
+    anoDeLancamento: 1972,
+    emCartaz: true
+  },
+  {
+    codigo: 2,
+    titulo: "O Mágico de Oz",
+    duracao: 1.86,
+    atores: ["Judy Garland", "Jack Haley", "Bert Lahr", "Frank Morgan"],
+    anoDeLancamento: 1939,
+    emCartaz: false
+  }
+]
